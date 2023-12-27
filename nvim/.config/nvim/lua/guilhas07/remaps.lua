6ebea6f25a0f520987a165ef7e88fd01eb7d006a
Guilherme Soares <guilhermesoares1970@gmail.com>
2023-12-27 15:17:04 +0000

    fix(neovim): remap "pv" aka project view keybinding to use oil

--- nvim/.config/nvim/lua/guilhas07/remaps.lua
+++ nvim/.config/nvim/lua/guilhas07/remaps.lua
@@ -2,7 +2,7 @@ local keymap = vim.keymap
 local opts = { noremap = true, silent = true }
 
 keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle, opts)
-keymap.set("n", "<leader>pv", vim.cmd.Ex, opts)
+keymap.set("n", "<leader>pv", vim.cmd.Oil, opts)
 keymap.set("n", "<leader>O", "zczA", opts)
 keymap.set("n", "<leader>z", vim.cmd.Zen, opts)
 -- keymap.set("n", "<c-d>", "<c-d>zz", opts)
