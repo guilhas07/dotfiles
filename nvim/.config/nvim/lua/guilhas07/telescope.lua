f0fdf5b2e31033d61d9b15740eb1855c85972edf
Guilherme Soares <guilhermesoares1970@gmail.com>
2024-01-22 20:55:52 +0000

    feat(neovim): Add custom telescope picker to reload private plugin

--- nvim/.config/nvim/lua/guilhas07/telescope.lua
+++ nvim/.config/nvim/lua/guilhas07/telescope.lua
@@ -1,7 +1,15 @@
+local telescope = require("telescope")
+
 local builtin = require("telescope.builtin")
 local themes = require("telescope.themes")
+local pickers = require("telescope.pickers")
+local finders = require("telescope.finders")
+local sorters = require("telescope.sorters")
+local config = require("telescope.config").values
+local actions = require("telescope.actions")
+local action_state = require("telescope.actions.state")
 
-require("telescope").setup({
+telescope.setup({
     pickers = {
         find_files = {
             mappings = {
@@ -15,9 +23,39 @@ require("telescope").setup({
     path_display = "smart",
 })
 
-require("telescope").load_extension("fzf")
+local private_plugin = {}
+local id = 1
+for _, plugin in ipairs(require("lazy").plugins()) do
+    -- local plugin
+    if plugin[1] == nil then
+        private_plugin[id] = plugin["name"]
+        id = id + 1
+    end
+end
+
+local reload_opts = {
+    prompt_title = "Reload Plugins",
+    finder = finders.new_table({
+        results = private_plugin,
+    }),
+    sorter = config.generic_sorter({}),
+    attach_mappings = function(prompt_bufnr, _)
+        actions.select_default:replace(function()
+            actions.close(prompt_bufnr)
+            local selected = action_state.get_selected_entry()
+            require("lazy.core.loader").reload(selected[1])
+        end)
+        return true
+    end,
+}
+
+telescope.load_extension("fzf")
 
 -- KeyMaps --
+vim.keymap.set("n", "<leader>pr", function()
+    pickers.new({}, reload_opts):find()
+end)
+
 vim.keymap.set("n", "gr", builtin.lsp_references)
 
 vim.keymap.set("n", "<leader>fb", builtin.buffers)
