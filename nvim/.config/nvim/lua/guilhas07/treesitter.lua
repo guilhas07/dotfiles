c9728d9faf25438da744712032b0d3b492c5e09c
Guilherme Soares <guilhermesoares1970@gmail.com>
2024-03-12 15:10:53 +0100

    feat(neovim): add treesitter-context navigation

--- nvim/.config/nvim/lua/guilhas07/treesitter.lua
+++ nvim/.config/nvim/lua/guilhas07/treesitter.lua
@@ -2,7 +2,11 @@ require("nvim-treesitter.configs").setup({
 	-- ensure_installed = "all",
 	highlight = { enable = true },
 	indent = { enable = true },
-    auto_install = true,
+	auto_install = true,
 	--incremental_selection = { enable = true },
 	--textobjects = { enable = true }
 })
+
+vim.keymap.set("n", "[c", function()
+	require("treesitter-context").go_to_context(vim.v.count1)
+end, { silent = true })
